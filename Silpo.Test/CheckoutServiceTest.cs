425bb72c8035472c9c4c5b5ee143657f8c428775
Shmyhol Roman <shmigol.roma@gmail.com>
2020-08-06 12:43:34 +0300

    Test for check auto open

--- Silpo.Test/CheckoutServiceTest.cs
+++ Silpo.Test/CheckoutServiceTest.cs
@@ -28,6 +28,22 @@ namespace Silpo.Test
             Check check = checkoutService.Close();
             Assert.Equal(10, check.GetTotalCost());
         }
+
+        [Fact]
+        public void addProducts__whenCheckIsClosed__openNewCheck()
+        {
+            CheckoutService checkoutService = new CheckoutService();
+            checkoutService.Open();
+            
+            checkoutService.AddProduct(new Product(7, "Milk"));
+            Check milkCheck = checkoutService.Close();
+            Assert.Equal(7, milkCheck.GetTotalCost());
+
+            checkoutService.AddProduct(new Product(3, "Bread"));
+            Check bredCheck = checkoutService.Close();
+            Assert.Equal(3, bredCheck.GetTotalCost());
+        }
+
         
     }
 }
